540e945c8ba7478eda1481134fa8968d30cfbecd
Sejal <sejalrparikh@gmail.com>
2022-06-29 00:29:18 -0400

    cleaning up repo

--- table-actions/src/app/app.functions/future.js
+++
@@ -1,8 +0,0 @@
-
-exports.main = async (context = {}, sendResponse) => {
-  console.log(context);
-
-  sendResponse({
-    message: `This could do something in the background in the future for example send an api request to another system or trigger a workflow in hubspot. Start dreaming because the future is here 🔮.`,
-  });
-};
