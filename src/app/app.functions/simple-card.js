f50fb8441a2a730615e342994543719709a6cc06
Sejal <sejalrparikh@gmail.com>
2022-07-13 13:12:28 -0400

    again?

--- src/app/app.functions/simple-card.js
+++ src/app/app.functions/simple-card.js
@@ -1,16 +1,34 @@
-const axios = require("axios");
+const axios = require('axios');
 
+
+/**
+ * Main "fetch function": see "webhooks" tab in the official docs:
+ * https://developers.hubspot.com/docs/api/crm/extensions/custom-cards
+ *
+ * Note: for the "sections" config that renders custom components, there's early
+ * documenation being gathered in the project repo for now:
+ * https://git.hubteam.com/HubSpot/ui-extensibility#shape-of-the-json-payload
+ */
 exports.main = async (context = {}, sendResponse) => {
-  // const { firstname, lastname } = context.propertiesToSend;
-  const sections = [
-    {
-      type: "text",
-      format: "markdown",
-      text: "Contact loves Tesla. He is planning to gift one to all of his family on his birthday, so look out for more deals",
-    },
-  ];
+    // const {
+    //     data: { message }
+    // } = await axios.get("https://dog.ceo/api/breeds/image/random");
 
-  sendResponse({
-    sections,
-  });
+    // const {
+    //     data: { fact }
+    // } = await axios.get("https://catfact.ninja/fact");
+
+    const sections = [
+       
+        {
+            "type":"text",
+            "format":"markdown",
+            "text": "**Like cats more? Here's some interesting cat fact for today!**"
+        },
+        
+    ];
+
+    sendResponse({
+        sections
+    });
 };
