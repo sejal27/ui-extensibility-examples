3b16c4671b87c11e843c0bf2948dd25142817771
Sejal <sejalrparikh@gmail.com>
2022-07-13 15:53:59 -0400

    changed text

--- src/app/app.functions/car-data.js
+++ src/app/app.functions/car-data.js
@@ -1,14 +1,5 @@
 const axios = require('axios');
 
-
-/**
- * Main "fetch function": see "webhooks" tab in the official docs:
- * https://developers.hubspot.com/docs/api/crm/extensions/custom-cards
- *
- * Note: for the "sections" config that renders custom components, there's early
- * documenation being gathered in the project repo for now:
- * https://git.hubteam.com/HubSpot/ui-extensibility#shape-of-the-json-payload
- */
 exports.main = async (context = {}, sendResponse) => {
     const { make, vin } = context.propertiesToSend;
     const apiUrl = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinExtended/" + vin + "?format=json"
