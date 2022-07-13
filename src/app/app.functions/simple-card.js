428be6cbd9b13607fe30645e2228882809ef9aa1
Sejal <sejalrparikh@gmail.com>
2022-07-13 13:06:42 -0400

    removed properties

--- src/app/app.functions/simple-card.js
+++ src/app/app.functions/simple-card.js
@@ -1,13 +1,5 @@
 const axios = require("axios");
 
-/**
- * Main "fetch function": see "webhooks" tab in the official docs:
- * https://developers.hubspot.com/docs/api/crm/extensions/custom-cards
- *
- * Note: for the "sections" config that renders custom components, there's early
- * documenation being gathered in the project repo for now:
- * https://git.hubteam.com/HubSpot/ui-extensibility#shape-of-the-json-payload
- */
 exports.main = async (context = {}, sendResponse) => {
   // const { firstname, lastname } = context.propertiesToSend;
   const sections = [
