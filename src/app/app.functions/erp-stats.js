037882dda1ca5680334f00b38536252e38a677b0
Sejal <sejalrparikh@gmail.com>
2022-08-15 02:02:28 -0400

    card js file fix

--- src/app/app.functions/erp-stats.js
+++ src/app/app.functions/erp-stats.js
@@ -1,34 +1,26 @@
 const axios = require("axios");
 
-/**
- * Main "fetch function": see "webhooks" tab in the official docs:
- * https://developers.hubspot.com/docs/api/crm/extensions/custom-cards
- *
- * Note: for the "sections" config that renders custom components, there's early
- * documenation being gathered in the project repo for now:
- * https://git.hubteam.com/HubSpot/ui-extensibility#shape-of-the-json-payload
- */
 exports.main = async (context = {}, sendResponse) => {
 	const sections = [
 		{
-			type: "statistics",
-			items: [
+			"type": "statistics",
+			"items": [
 				{
-					label: "Views",
-					number: "347",
-					description: {
-						type: "trend",
-						value: "23.36%",
-						direction: "increase",
+					"label": "Views",
+					"number": "347",
+					"description": {
+						"type": "trend",
+						"value": "23.36%",
+						"direction": "increase",
 					},
 				},
 				{
-					label: "Total Submissions",
-					number: "45",
-					description: {
-						type: "trend",
-						value: "9.25%",
-						direction: "decrease",
+					"label": "Total Submissions",
+					"number": "45",
+					"description": {
+						"type": "trend",
+						"value": "9.25%",
+						"direction": "decrease",
 					},
 				},
 			],
