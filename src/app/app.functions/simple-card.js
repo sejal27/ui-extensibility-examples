88d0e00b490130a22b7ad1cd920a1f3854500ff5
sejal27 <sejalrparikh@gmail.com>
2022-08-19 13:14:31 -0400

    asana tasks

--- src/app/app.functions/simple-card.js
+++ src/app/app.functions/simple-card.js
@@ -1,32 +1,62 @@
-const axios = require('axios');
+const axios = require("axios");
+const asana = require("asana");
 
 exports.main = async (context = {}, sendResponse) => {
-    const {firstname} = context.propertiesToSend;
-    const sections = [
-       
-        {
-            "type":"text",
-            "format":"markdown",
-            "text": "This contact loves Tesla!**"
-        },
-        {
-            "type": "divider",
-            "distance": "small"
-        },
-        {
-            "type": "text",
-            "format": "markdown",
-            "text": "**All Contacts**"
-        },
-        {
-            type: 'crm::table',
-            objectTypeId: '0-1',
-            properties: ['firstname', 'lastname', 'email'],
-            pageSize: 3
-        }, 
-    ];
-
-    sendResponse({
-        sections
+  const client = asana.Client.create().useAccessToken(process.env.ASANA_PAT);
+  if (context.event && context.event.type === "SUBMIT") {
+    client.tasks.createTask({
+      name: context.event.payload.formState.taskName,
+      notes: context.event.payload.formState.taskNotes,
+      projects: ["1202716730818738"],
+      pretty: true,
     });
+  }
+
+  const sections = [
+    {
+      type: "text",
+      format: "markdown",
+      text: "This contact loves Tesla!**",
+    },
+    {
+      type: "divider",
+      distance: "small",
+    },
+    {
+      type: "text",
+      format: "markdown",
+      text: "**All Contacts**",
+    },
+    {
+      type: "form",
+      content: [
+        {
+          type: "input",
+          name: "taskName",
+          inputType: "text",
+          label: "Task Name",
+          initialValue: "",
+        },
+        {
+          type: "input",
+          name: "taskNotes",
+          inputType: "text",
+          label: "Task Notes",
+          initialValue: "",
+        },
+        {
+          type: "button",
+          text: "Submit Form",
+          onClick: {
+            type: "SUBMIT",
+            serverlessFunction: "simple-card",
+          },
+        },
+      ],
+    },
+  ];
+
+  sendResponse({
+    sections,
+  });
 };
